d62b53fc0e293023021d6c620096584e4ef312be
preethi123455 <preethiusha007@gmail.com>
2025-12-03 21:41:36 +0530

    6

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -16,7 +16,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:5000/signup", {
+    const response = await fetch("https://visual-math-oscg.onrender.com/signup", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
